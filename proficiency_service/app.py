b9dd070a93b23725378ab88ed49e8fd915af5c9f
Matt Nguyen <nguyentrongminhnghia@gmail.com>
2024-04-09 17:19:47 -0400

    Added Flask microservice for dertermining a text's level

---
+++ proficiency_service/app.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, jsonify
+import numpy as np
+import joblib
+
+app = Flask(__name__)
+
+modelPath = "../actfl_proficiency_classifier/trained_models/proficiency_classifier.joblib"
+model = joblib.load(modelPath)
+
+@app.route("/predict_prof", methods=["POST"])
+def predict_prof():
+    try:
+        # Get data from request as JSON
+        data = request.get_json()
+        userText = data["text"]
+
+        prediction = model.predict(userText)
+        # Convert to List to be able to jsonify
+        predictionList = prediction.tolist() if isinstance(prediction, np.ndarray) else prediction
+
+
+        response = {
+            "prediction": predictionList
+        }
+        
+        return jsonify(response), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0", port=6969)
