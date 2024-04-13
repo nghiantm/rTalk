55feaa2a3aea4f8de1a1c5f902620fd80f9bd31d
Matt Nguyen <nguyentrongminhnghia@gmail.com>
2024-04-13 06:38:49 -0400

    Added method for completing response

--- proficiency_service/app.py
+++ proficiency_service/app.py
@@ -1,11 +1,17 @@
 from flask import Flask, request, jsonify
 import numpy as np
 import joblib
+from openai import OpenAI
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 
 modelPath = "../actfl_proficiency_classifier/trained_models/proficiency_classifier.joblib"
 model = joblib.load(modelPath)
+
+client = OpenAI(api_key=os.environ.get("OPENAI_API_KEY"))
 
 @app.route("/", methods=["GET"])
 def home():
@@ -30,3 +36,23 @@ def predict_prof():
         return jsonify(response), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
+    
+@app.route("/generate_response", methods=["POST"])
+def generate_response():
+    data = request.get_json()
+    messages = data["messages"]
+    try:
+        response = client.chat.completions.create(
+            model="gpt-3.5-turbo-0125",
+            messages=messages,
+            temperature=0
+        )
+
+        message = response.choices[0].message.content
+
+        return jsonify(message), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    
+if __name__ == "__main__":
+    app.run()
